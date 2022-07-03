68e1c72560d0b04f85fe1e24dc91d278c5f8846b
lihu132 <lihu132@gmail.com>
2022-07-03 16:58:53 +1000

    jshint_ignore

--- react-complete-guide/src/index.js
+++ react-complete-guide/src/index.js
@@ -1,7 +1,9 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+/* jshint ignore:start */
 root.render(<App />);
+/* jshint ignore:end */
