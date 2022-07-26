eb6c10ac120fa3ccee6ef75e869ffb40d6e92348
leo-outside <morinbrit@gmail.com>
2022-07-26 15:31:29 +1000

    event handler

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -3,17 +3,24 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const expenseDate = new Date(2022, 5, 27);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+  // const expenseDate = new Date(2022, 5, 27);
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 294.67;
+  let title = props.title;
+
+  const clickHandler = () => {
+      title ='Updated!';
+      console.log(title);
+  }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick= {clickHandler}>Change title</button>
     </Card>
   );
 }
