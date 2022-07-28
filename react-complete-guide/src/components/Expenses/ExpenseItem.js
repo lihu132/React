b773016ed7b018dcb8e39f144692690b2fb1676e
lihu132 <lihu132@gmail.com>
2022-07-28 23:45:09 +1000

    state

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
@@ -10,24 +10,24 @@ const ExpenseItem = (props) => {
   // const expenseAmount = 294.67;
 
   //useState return 2 elements, first is value itself, second is updating function
-  const [title, setTitle] = useState(props.title);
-  console.log('ExpenseItem evaluated by React');
+  // const [title, setTitle] = useState(props.title);
+  // console.log('ExpenseItem evaluated by React');
 
-  const clickHandler = () => {
-      setTitle('Updated!');
-      console.log(title);
-  }
+  // const clickHandler = () => {
+  //     setTitle('Updated!');
+  //     console.log(title);
+  // }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick= {clickHandler}>Change title</button>
+      {/* <button onClick= {clickHandler}>Change title</button> */}
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
